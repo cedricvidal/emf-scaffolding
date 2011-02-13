16986f8bc669972505d13e6c6af6fbfb79715771
Cédric Vidal <cedric@vidal.biz>
2011-02-13 14:32:46 +0100

    StoreAsideStrategy could be made stateless.

--- plugins/org.eclipselabs.emf.scaffolding.tests/src/org/eclipselabs/emf/scaffolding/tests/status/strategy/StoreAsideStrategy.java
+++ plugins/org.eclipselabs.emf.scaffolding.tests/src/org/eclipselabs/emf/scaffolding/tests/status/strategy/StoreAsideStrategy.java
@@ -11,8 +11,6 @@
  *******************************************************************************/
 package org.eclipselabs.emf.scaffolding.tests.status.strategy;
 
-import java.io.IOException;
-
 import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
@@ -26,26 +24,56 @@ import org.eclipselabs.emf.scaffolding.runtime.status.ScaffoldingContext;
 import org.eclipselabs.emf.scaffolding.session.util.ScaffoldingStatusPruner;
 
 /**
- * Requires EMF 3.7 because of https://bugs.eclipse.org/bugs/show_bug.cgi?id=336775
+ * Store scaffolded elements in a side resource. Scaffolding status is not
+ * stored, it stays transient and is reestablished when loading scaffolded
+ * elements from side resource.
+ * 
+ * Requires EMF 3.7 because of
+ * https://bugs.eclipse.org/bugs/show_bug.cgi?id=336775
  * 
  * @author cvidal
- *
+ * 
  */
 public class StoreAsideStrategy extends AbstractScaffoldingStatusStorageStrategy {
 
 	private ScaffoldingStatusPruner pruner = new ScaffoldingStatusPruner();
-	private Resource changedescriptionResource;
-	private ChangeDescription changeDescription;
 
+	/**
+	 * Tries to load a {@link ChangeDescription} containing scaffolded
+	 * elements stored during previous save.
+	 * 
+	 * If available, applies the {@link ChangeDescription} to the model.
+	 * 
+	 * We want the elements created from the {@link ChangeDescription} to have
+	 * their scaffolding status set to true. We therefore need to apply the
+	 * {@link ChangeDescription} in a ScaffoldingSession but we don't want
+	 * scaffolding to fire, so we need to deactivate immediate fire while
+	 * applying the {@link ChangeDescription}. Immediate fire is restored after
+	 * load.
+	 * 
+	 * Does nothing if a {@link #changeDescription} hasn't be found.
+	 */
 	@Override
-	public void afterLoad(EObject application) {
-		FactPublisher factPublisher = ScaffoldingExecutionEnvironment.getFactPublisher(application);
+	public void afterLoad(EObject model) {
+		Resource changedescriptionResource = null;
+		try {
+			ResourceSet resourceSet = model.eResource().getResourceSet();
+			changedescriptionResource = resourceSet.getResource(URI.createURI(FS_ROOT + "changedesc.xmi"), true);
+		} catch (Exception e) {
+			changedescriptionResource = null;
+		}
+		ChangeDescription changeDescription = null;
+		if(changedescriptionResource != null) {
+			changeDescription = (ChangeDescription) changedescriptionResource.getContents().get(0);
+		}
 
-		// We want the elements created from the changeDesc to have their
-		// scaffolding status set to true. We therefore need to apply the
-		// changeDesc in a ScaffoldingSession
-		// but we don't want scaffolding to fire, so we need to deactivate
-		// immediate fire while applying the changeDesc
+		if(changeDescription == null) {
+			return;
+		}
+
+		FactPublisher factPublisher = ScaffoldingExecutionEnvironment.getFactPublisher(model);
+
+		boolean immediateFire = factPublisher.isImmediateFire();
 		factPublisher.setImmediateFire(false);
 
 		final ChangeDescription desc = changeDescription;
@@ -56,25 +84,14 @@ public class StoreAsideStrategy extends AbstractScaffoldingStatusStorageStrategy
 			}
 		});
 
-		factPublisher.setImmediateFire(true);
-	}
-
-	@Override
-	public void beforeLoad(ResourceSet resourceSet)
-			throws IOException {
-		try {
-			changedescriptionResource = resourceSet.getResource(URI.createURI(FS_ROOT + "changedesc.xmi"), true);
-		} catch (Exception e) {
-			changedescriptionResource = null;
-		}
+		factPublisher.setImmediateFire(immediateFire);
 		changeDescription = null;
-		if(changedescriptionResource == null) {
-			changedescriptionResource = resourceSet.createResource(URI.createURI(FS_ROOT + "changedesc.xmi"));
-		} else {
-			changeDescription = (ChangeDescription) changedescriptionResource.getContents().get(0);
-		}
 	}
 
+	/**
+	 * Prunes scaffolded elements from {@link ResourceSet} and stores them in a
+	 * {@link ChangeDescription}.
+	 */
 	@Override
 	public void beforeSave(ResourceSet rs) {
 
@@ -86,7 +103,8 @@ public class StoreAsideStrategy extends AbstractScaffoldingStatusStorageStrategy
 			}
 		}
 
-		changeDescription = pruner.prune(rs);
+		ChangeDescription changeDescription = pruner.prune(rs);
+		Resource changedescriptionResource = rs.createResource(URI.createURI(FS_ROOT + "changedesc.xmi"));
 		changedescriptionResource.getContents().add(changeDescription);
 	}
 
